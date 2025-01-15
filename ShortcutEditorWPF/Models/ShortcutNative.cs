5c8d924d88381259ca24c40099ead171bba2033d
Dolonas <centaprisme@gmail.com>
2025-01-15 13:32:06 +0400

    new model : ShortcutNative

---
+++ ShortcutEditorWPF/Models/ShortcutNative.cs
@@ -0,0 +1,17 @@
+﻿using ShellLink;
+
+namespace ShortcutEditorWPF.Models;
+
+public class ShortcutNative
+{
+    internal Shortcut InternalShortcut { get; set; }
+
+    public ShortcutNative(Shortcut internalShortcut)
+    {
+        InternalShortcut = internalShortcut;
+    }
+
+    public ShortcutNative() : this(new Shortcut())
+    {
+    }
+}
